6a3c5471877fb4c611ad761e99c6061e831568f3
Leonardo Guimarães <82131370+Leonardo-V-Guimaraes@users.noreply.github.com>
2022-04-27 21:27:58 -0300

    Aula 2

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -10,7 +10,15 @@ print("Você digitou " , chute_str)
 
 chute = int(chute_str)
 
-if (numero_secreto == chute):
+acertou     = chute == numero_secreto
+chute_maior = chute > numero_secreto
+chute_menor = chute < numero_secreto
+
+if (acertou):
     print("Você acertou!")
 else: 
-    print("Você errou!")
+    if(chute_maior):
+        print("Você errou! O seu chute foi maior do que o número secreto.")
+    elif(chute_menor):
+        print("Você errou! O seu chute foi menor do que o número secreto.")
+print ("Fim do jogo!")
