499bcf3b5355ee112194435ff6a2574823786371
Leonardo Guimarães <82131370+Leonardo-V-Guimaraes@users.noreply.github.com>
2022-04-28 20:09:30 -0300

    Aula 5

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -1,12 +1,26 @@
+import random
+
 print("*********************************")
 print("Bem vindo ao jogo de adivinhação!")
 print("*********************************")
 
-numero_secreto = 42
-total_de_tentivas = 3
+numero_secreto = random.randrange(1,101)
+total_de_tentivas = 0
+
+print("Selecione o nível de dificuldade")
+print("(1) Fácil (2) Médio (3) Difícil")
+
+nivel = int(input("Define o nível: "))
+
+if(nivel == 1):
+    total_de_tentivas = 20
+elif(nivel == 2):
+    total_de_tentivas = 10
+else:
+    total_de_tentivas = 5
 
 for rodada in range(1, total_de_tentivas + 1):
-    print("Tentativa {} de {}".format(rodada, total_de_tentivas,"\n"))
+    print("\n""Tentativa {} de {}".format(rodada, total_de_tentivas,"\n"))
     chute_str = input("Digite o seu numero: ")
     print("Você digitou: " , chute_str)
     chute = int(chute_str)
